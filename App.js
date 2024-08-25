58a02620cd83cb732e109d4536de3164fbca32bb
JoaoRodrigues1803 <joao1803208@gmail.com>
2024-08-25 14:20:37 -0300

    Criando base

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Text, View, Button, Image } from 'react-native';
 
-export default function App() {
+import javascriptImg from './assets/javascript.png'
+import estilo from './estilo';
+
+export default function TelaJavaScript (props) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View >
+     
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
