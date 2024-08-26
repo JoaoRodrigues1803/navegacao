f731b92d3926de3963a904a95a220701a25bb7c2
JoaoRodrigues1803 <joao1803208@gmail.com>
2024-08-26 01:12:29 -0300

    fimm

--- App.js
+++ App.js
@@ -1,13 +1,30 @@
-import React from 'react';
 import { Text, View, Button, Image } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
-import javascriptImg from './assets/javascript.png'
-import estilo from './estilo';
+const MenuNav = createDrawerNavigator
 
-export default function TelaJavaScript (props) {
+import TelaJavaScript from  "./componentes/TelaJavaScript"
+import TelaNode from "./componentes/TelaNode"
+import TelaReact from "./componentes/TelaReact"
+
+export default function App() {
   return (
-    <View >
-     
-    </View>
+    <NavigationContainer>
+      <MenuNav.Navigator>
+        <MenuNav.Screen name="Java Script" components={TelaJavaScript} />
+        <MenuNav.Screen name="Node" components={TelaNode} />
+        <MenuNav.Screen name="React" components={TelaReact} />
+      </MenuNav.Navigator>
+    </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItens: 'center',
+    justifyContent: 'center'
+    },
+});
--- componentes/TelaJavaScript.js
+++ componentes/TelaJavaScript.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Text, View, Button, Image } from 'react-native';
 
-import javascriptImg from './assets/javascript.png'
+import javascriptImg from '../assets/javascript.png'
 import estilo from './estilo';
 
 export default function TelaJavaScript (props) {
